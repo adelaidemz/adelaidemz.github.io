83e4ae74033f820cf2fa09bf599cbb931729740f
Adelaide Zhang <adelaidemzhang@gmail.com>
2023-12-04 21:24:24 -0500

    testing paper.js without build

---
+++ papertest.js
@@ -0,0 +1,29 @@
+
+// import Paper from 'paper'
+// Create a Paper.js Path to draw a line into it:
+// var path = new Path();
+// path.strokeColor = 'white';
+
+// var start = new Point(100, 100);
+// // path.add(start);
+// // Move to start and draw a line from there
+// path.moveTo(start);
+// // Note the plus operator on Point objects.
+// // PaperScript does that for us, and much more!
+// path.lineTo(start + [ 100, 50 ]);
+
+// var bluePoint = new Paper.Point(120, 70);
+// var blueCircle = new Paper.Path.Circle(bluePoint, 50);
+// blueCircle.fillColor = 'blue';
+
+// var redPoint = new Paper.Point(180, 70);
+// var redCircle = new Paper.Path.Circle(redPoint, 50);
+// redCircle.fillColor = 'red';
+
+var bluePoint = new Point(120, 70);
+var blueCircle = new Path.Circle(bluePoint, 50);
+blueCircle.fillColor = 'blue';
+
+var redPoint = new Point(180, 70);
+var redCircle = new Path.Circle(redPoint, 50);
+redCircle.fillColor = 'red';

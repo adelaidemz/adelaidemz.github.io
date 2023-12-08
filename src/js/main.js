25f60f320858233fe36611cae4b1e96e79c6df06
Adelaide Zhang <adelaidemzhang@gmail.com>
2023-12-07 22:12:56 -0500

    add routing to vite config, final? restructure

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ const cards = [
 	image: '/rpart-dt.png',
 	text: `Machine learning project to classify visible night sky bodies 
 	 (KNN, random forest, AdaBoost)`, 
-	link: 'stellar/index.html',
+	link: 'pages/stellar',
 	},
   { title: 'Artist collabs (WIP)', 
 	image: '/collab-card-img.png',
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import path from 'path'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default {
-  root: path.resolve(__dirname, 'src'),
-  base: '/'
-}
-
+export default defineConfig ({
+  root: resolve(__dirname, 'src'),
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        stellar: resolve(__dirname, 'src/pages/stellar.html'),
+        testpage: resolve(__dirname, 'src/testpage.html'),
+        test: resolve(__dirname, 'src/test.html'),
+      },
+    },
+  },
+})

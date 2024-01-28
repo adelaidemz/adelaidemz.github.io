6528fa0ef2cf40603a298627c121a240c94016c7
Adelaide Zhang <adelaidemzhang@gmail.com>
2024-01-28 18:01:30 -0500

    remove test pages

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,6 @@ export default defineConfig ({
         main: resolve(__dirname, 'src/index.html'),
         stellar: resolve(__dirname, 'src/pages/stellar.html'),
         markov: resolve(__dirname, 'src/pages/markov.html'),
-        testpage: resolve(__dirname, 'src/testpage.html'),
-        test: resolve(__dirname, 'src/test.html'),
       },
     },
   },

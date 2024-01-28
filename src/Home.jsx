938112ebc95727704780cc3b809ecebfddea2a70
Adelaide Zhang <adelaidemzhang@gmail.com>
2024-01-28 12:29:37 -0500

    move card object into separate file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,39 +1,4 @@
-import ProjectCard from "./ProjectCard.jsx"
-import Resume from "./pages/resume.jsx"
-import { Button, Heading, IconButton, SimpleGrid, Text, Stack,
-Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import { EmailIcon, ExternalLinkIcon } from '@chakra-ui/icons'
-
-const cards = [
-    { title: 'Sorting hues', 
-      image: '/sorting-card-img.png',
-      text: 'A colorful visualizer of some iterative sorting algorithms. React, Typescript, and Vite', 
-      link: 'https://adelaidemz.github.io/sorting', 
-      source_link: 'https://github.com/adelaidemz/sorting',
-      },
-    { title: 'Stellar classification', 
-      image: '/rpart-dt.png',
-      text: `Machine learning project to classify visible night sky bodies 
-       (KNN, random forest, AdaBoost)`, 
-      link: 'pages/stellar',
-      },
-    { title: 'Markov chain name generation', 
-        image: '/markov.png',
-        text: 'Uses a markov chain to generate stylized names using Javascript.', 
-        link: 'pages/markov', 
-      },
-    { title: 'API Microservices (FreeCodeCamp)', 
-        image: '/fcc.png',
-        text: 'Projects for FreeCodeCamp\'s Backend Development and APIs course. Node and Express, hosted on Render.', 
-        link: 'https://microservices-fcc-ejkz.onrender.com', 
-      },
-    { title: 'Artist collabs (WIP)', 
-      image: '/collab-card-img.png',
-      text: 'Network view of musical artist collaborations. React and D3.', 
-      link: 'https://adelaidemz.github.io/artist-collab', 
-      source_link: 'https://github.com/adelaidemz/artist-collab',
-      },
-  ]
+import cards from './data/page-info.json';
 
 export default function Home() {    
     return (

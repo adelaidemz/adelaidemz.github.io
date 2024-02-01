75c6147e058f419343aafaac4373aa88372448b7
Adelaide Zhang <adelaidemzhang@gmail.com>
2024-02-01 00:50:49 -0500

    update extended theme and fonts

--- src/index.jsx
+++ src/index.jsx
@@ -1,75 +1,41 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import { Global } from "@emotion/react"
+import Home from "./Home.jsx";
 
-const config = {
+const colorTheme = {
   // auto light/dark mode based on system
   initialColorMode: 'system',
   useSystemColorMode: true,
+}
 
-  // fonts: {
-  //   heading: 'Open Sans, sans-serif',
-  //   text: 'Georgia, serif',
-  // },
-  components: {
-    Button: {
-      // 1. We can update the base styles
-      baseStyle: {
-        fontWeight: 'bold', // Normally, it is "semibold"
-      },
-      // 2. We can add a new button size or extend existing
-      sizes: {
-        xl: {
-          h: '56px',
-          fontSize: 'lg',
-          px: '32px',
-        },
-      },
-      // 3. We can add a new visual variant
-      variants: {
-        'with-shadow': {
-          bg: 'red.400',
-          boxShadow: '0 0 2px 2px #efdfde',
-        },
-        // 5. We can add responsive variants
-        sm: {
-          bg: 'teal.500',
-          fontSize: 'md',
-        },
-      },
-      // 6. We can overwrite defaultProps
-      defaultProps: {
-        size: 'lg', // default is md
-        variant: 'sm', // default is solid
-        colorScheme: 'green', // default is gray
-      },
-    },
+const config = extendTheme({
+  colorTheme,
+  fonts: {
+    heading: 'Inter, sans-serif',
+    text: 'Circular Std Book, sans-serif',
   },
-    // link
-    Link: {
-      baseStyle: {
-        textDecoration: 'underline',
-        color: 'blue.500',
+  components: {
+      Button: {
+          variants: {
+              customLink: {
+                  fontWeight: 'light',
+                  borderRadius: 50,
+                },
+          }
       },
-      // defaultProps: {
-      //   size: 'lg', // default is md
-      //   variant: 'sm', // default is solid
-      //   colorScheme: 'green', // default is gray
-      // },
-    }
+      Heading: {
+          baseStyle: {
+              fontWeight: 500,
+          }
+      },
   }
-  
-
-const theme = extendTheme({ config })
-// import "./styles.css";
-
-import Home from "./Home.jsx";
+})
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={config}>
       <Home />
     </ChakraProvider>
   </StrictMode>
